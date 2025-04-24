378851d677292a7a5504016abad56f5295236c8e
Elyse Iradukunda <ellycreativity8@gmail.com>
2025-04-24 21:05:11 +0200

    last push tonight

--- readFile.js
+++ readFile.js
@@ -1,6 +1,18 @@
 import fs from "fs";
+import { readFile } from 'fs/promises';
 
-fs.readFile('./text.txt', 'utf8' , (err,data)=>{
-    if(err) throw err;
-    console.log(data);
-});
+// fs.readFile('./text.txt', 'utf8' , (err,data)=>{
+//     if(err) throw err;
+//     console.log(data);
+// });
+
+
+// doing it using simple way  readFileSync()
+
+// const data = fs.readFileSync('./text.txt', 'utf8');
+// console.log(data);
+// using promises .then() and .catch()
+
+readFile('./text.txt', 'utf8')
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
